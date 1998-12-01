8d4895de898548cfdc7d6a3ea3b94af66aaedbed
mt <>
1998-12-01 23:48:37 +0000

    fixed that lastread thing

--- makefile
+++ makefile
@@ -3,8 +3,7 @@ COPT = -g -O2 -DUNIX -I../smapi -I../fidoconfig
 INSTBINDIR = /usr/local/bin
 INSTMANDIR = /usr/local/man
 
-all : sqpack \
-      install
+all : sqpack 
 
 sqpack: sqpack.c
 	$(CC) $(COPT) sqpack.c -o sqpack -lsmapilnx -lfidoconfig
@@ -15,3 +14,11 @@ sqpack.1.gz : sqpack.1
 install: sqpack sqpack.1.gz
 	install -s sqpack $(INSTBINDIR)
 	install sqpack.1.gz $(INSTMANDIR)/man1
+
+clean: 
+	-rm sqpack
+	-rm sqpack.1.gz
+	-rm *~
+
+distclean: clean
+
--- sqpack.c
+++ sqpack.c
@@ -1,16 +1,123 @@
+#include <stdio.h>
+#include <errno.h>
 #include <time.h>
+
+#ifdef UNIX
+#include <unistd.h>
+#else
+#include <io.h>
+#endif
+
+#include <fcntl.h>
+#include <sys/stat.h>
+
 #include <msgapi.h>
 #include <fidoconfig.h>
+#include <common.h>
 
-void processMsg(dword msgNum, HAREA oldArea, HAREA newArea, s_area area )
+unsigned long msgCopied; // per Area
+
+void readLastreadFile(char *fileName, UINT32 *lastread[], HAREA area)
+{
+   int fd;
+   struct stat st;
+   unsigned long noOfElements, i, temp;
+   unsigned char buffer[4];
+   char *name;
+
+   name = (char *) malloc(strlen(fileName)+4+1);
+   strcpy(name, fileName);
+   strcat(name, ".sql");
+   
+   fd = sopen(name, O_BINARY | O_RDWR, SH_DENYNO, S_IWRITE | S_IREAD);
+   if (fd != -1) {
+      
+      fstat(fd, &st);
+      noOfElements = st.st_size / sizeof(UINT32);
+      *lastread = (UINT32 *) malloc(noOfElements);
+      
+      for (i = 0; i < noOfElements; i++) {
+         read(fd, &buffer, 4);
+         temp = buffer[0] + (((unsigned long)(buffer[1])) << 8) +
+                (((unsigned long)(buffer[2])) << 16) +
+                (((unsigned long)(buffer[3])) << 24);
+         (*lastread)[i] = MsgUidToMsgn(area, temp, UID_NEXT);
+      }
+
+      close(fd);
+      
+   } else *lastread = NULL;
+
+   free(name);
+}
+
+void writeLastreadFile(char *fileName, UINT32 *lastread[], HAREA area)
+{
+   char *name;
+   unsigned char buffer[4];
+   int fd;
+   unsigned long i, noOfElements, temp;
+   
+   
+   if (*lastread != NULL) {
+
+      name = (char *) malloc(strlen(fileName)+4+1);
+      strcpy(name, fileName);
+      strcat(name, ".sql");
+
+      fd = sopen(name, O_BINARY | O_RDWR, SH_DENYNO, S_IWRITE | S_IREAD);
+
+      if (fd != -1) {
+
+         lseek(fd, 0l, SEEK_SET);
+
+         noOfElements = sizeof(*lastread) / sizeof(UINT32);
+
+         for (i = 0; i < noOfElements; i++) {
+
+            temp = MsgMsgnToUid(area, (*lastread)[i]);
+            
+            buffer[0] = temp & 0xFF;
+            buffer[1] = (temp >> 8) & 0xFF;
+            buffer[2] = (temp >> 16) & 0xFF;
+            buffer[3] = (temp >> 24) & 0xFF;
+
+            write(fd, &buffer, 4);
+         }
+
+         close(fd);
+              
+      } else printf("Could not write lastread file %s, error %u!\n", name, errno);
+      
+      free(name);
+   }
+}
+
+unsigned long getOffsetInLastread(UINT32 *lastread[], dword msgnum ) {
+
+   unsigned long i, noOfElements;
+
+   noOfElements = sizeof(*lastread) / sizeof(UINT32);
+   
+   for (i = 0; i < noOfElements; i++) {
+      if ((*lastread)[i] == msgnum) return i;
+   }
+
+   return (-1);
+   
+}
+
+void processMsg(dword msgNum, HAREA oldArea, HAREA newArea, s_area area, UINT32 *oldLastread[], UINT32 *newLastread[] )
 {
    HMSG msg, newMsg;
    XMSG xmsg;
-   static int msgProcessed = 0;
+   static unsigned long msgProcessed = 0;
    struct tm tmTime;
    time_t ttime, actualTime = time(NULL);
    char *text, *ctrlText;
-   dword  textLen, ctrlLen;
+   dword  textLen, ctrlLen, oldMsgnum, newMsgnum;
+   
+   unsigned long offset;
 
    msg = MsgOpenMsg(oldArea, MOPEN_RW, msgNum);
    if (msg == NULL) return;
@@ -32,6 +139,19 @@ void processMsg(dword msgNum, HAREA oldArea, HAREA newArea, s_area area )
          newMsg = MsgOpenMsg(newArea, MOPEN_CREATE, 0);
          MsgWriteMsg(newMsg, 0, &xmsg, text, textLen, textLen, ctrlLen, ctrlText);
          MsgCloseMsg(newMsg);
+
+         msgCopied++;
+
+         if (*oldLastread != NULL) {
+
+            oldMsgnum = msgNum;
+            newMsgnum = msgCopied;
+            
+            if ((offset = getOffsetInLastread(oldLastread, oldMsgnum)) != (-1)) {
+               // printf("%u\n", newMsgnum);
+               (*newLastread)[offset] = newMsgnum;
+            }
+         }
 
          free(text);
          free(ctrlText);
@@ -57,9 +177,6 @@ void renameArea(char *oldName, char *newName)
    remove(oldTmp);
    rename(newTmp, oldTmp);
 
-   oldTmp[strlen(oldTmp)-1] = 'l';
-   remove(oldTmp);
-
    oldTmp[strlen(oldTmp)-1] = 'i';
    newTmp[strlen(newTmp)-1] = 'i';
    remove(oldTmp);
@@ -75,7 +192,9 @@ void purgeArea(s_area area)
    char *oldName = area.fileName;
    char *newName;
    HAREA oldArea, newArea;
-   dword highMsg, i;
+   dword highMsg, i, numMsg;
+
+   UINT32 *oldLastread, *newLastread = NULL;
 
    //generated tmp-FileName
    newName = (char *) malloc(strlen(oldName)+4+1);
@@ -88,14 +207,27 @@ void purgeArea(s_area area)
    if ((oldArea != NULL) && (newArea != NULL)) {
 
       highMsg = MsgGetHighMsg(oldArea);
+      numMsg = MsgGetNumMsg(oldArea);
+      readLastreadFile(oldName, &oldLastread, oldArea);
+      if (oldLastread != NULL) {
+         newLastread = (UINT32 *) malloc(sizeof(oldLastread));
+         memcpy(newLastread, oldLastread, sizeof(newLastread));
+      }
 
       for (i = 1; i <= highMsg; i++) {
          
-         processMsg(i, oldArea, newArea, area);
+         processMsg(i, oldArea, newArea, area, &oldLastread, &newLastread);
       }
+
+      writeLastreadFile(oldName, &newLastread, newArea);
 
       MsgCloseArea(oldArea);
       MsgCloseArea(newArea);
+
+      printf("   oldMsg: %u   newMsg: %u\n", numMsg, msgCopied);
+
+      free(oldLastread);
+      free(newLastread);
 
       //rename oldArea to newArea
       renameArea(oldName, newName);
@@ -111,7 +243,7 @@ int main() {
    int i;
    struct _minf m;
    
-   printf("sqpack v0.9.9\n");
+   printf("sqpack v1.0.0\n");
 
    cfg = readConfig();
 
@@ -127,6 +259,7 @@ int main() {
          // purge areas
          if ((cfg->echoAreas[i].msgbType & MSGTYPE_SQUISH) == MSGTYPE_SQUISH) {
             printf("%s\n", cfg->echoAreas[i].areaName);
+            msgCopied = 0;
             purgeArea(cfg->echoAreas[i]);
          }
       }
