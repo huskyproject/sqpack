778c35e538509773987b2381cc9f97e959f87a91
ml <>
2000-10-24 16:47:00 +0000

    *** empty log message ***

--- sqpack.c
+++ sqpack.c
@@ -157,35 +157,6 @@ void SqWriteLastreadFile(char *fileName, UINT32 *lastread, ULONG lcount,
     ((word)((unsigned char)(ptr)[0]) |         \
      (((word)((unsigned char)(ptr)[1])) << 8 ))
 
-/*
- *  put_dword
- *
- *  Writes a 4 byte word in little endian notation, independent of the local
- *  system architecture.
- */
-
-void put_dword(byte *ptr, dword value)
-{
-    ptr[0] = (value & 0xFF);
-    ptr[1] = (value >> 8) & 0xFF;
-    ptr[2] = (value >> 16) & 0xFF;
-    ptr[3] = (value >> 24) & 0xFF;
-}
-
-/*
- *  put_word
- *
- *  Writes a 4 byte word in little endian notation, independent of the local
- *  system architecture.
- */
-
-void put_word(byte *ptr, word value)
-{
-    ptr[0] = (value & 0xFF);
-    ptr[1] = (value >> 8) & 0xFF;
-}
-
-
 typedef struct
 {
     unsigned long UserCRC;         /* CRC-32 of user name (lowercase) */
